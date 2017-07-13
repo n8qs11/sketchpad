ab2772dcfdc1bfc0bd825730acc03ec83aeb1652
Nate Soo <n8qs11@gmail.com>
2017-07-12 23:10:36 -0700

    finished the project js, html, most of css

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  var color = "black";
+  var dim = 500;
+  function generatePad(size) {
+    $("div.pad").empty();
+    var cellDim = dim / size;
+    for (var i = 0; i < size; i++) {
+      $("div.pad").append("<div class='row' id='" + i +"'></div>");
+      for (var j = 0; j < size; j++) {
+        $("#" + i).append("<div class='cell'></div>");
+      }
+    }
+    $(".row").css("height", cellDim + "px");
+    $(".cell").css("height", (cellDim - 1) + "px");
+    $(".cell").css("width", (cellDim - 1) + "px");
+  }
+  generatePad(16);
+  $("#clear").click(function() {
+    $(".cell").css("background-color", "white");
+  });
+  $("#colorPick").click(function() {
+    color = prompt("Please type a color")
+  });
+  $("#size").click(function() {
+    generatePad(prompt("grid size"));
+  });
+  $(document).on("mouseenter", ".cell", function() {
+    $(this).css("background-color", color);
+  });
+});
